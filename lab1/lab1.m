e452a49297100f59c472266ce24662dd14bf6624
jamesclyeh <jamesclyeh@gmail.com>
2014-09-10 22:25:06 -0400

    Add lena loading and some fixes.

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -4,8 +4,13 @@ close all; clear; clc;
 % Load images
 %========================================
 
-tire = imread('tire.tiff')
-tire_noisy = imnoise(tire, 'gaussian')
-cameraman = imread('cameraman.tiff')
-cameraman_noisy = imread(cameraman, 'gaussian')
+tire = imread('tire.tif');
+tire_noisy = imnoise(tire, 'gaussian');
+cameraman = imread('cameraman.tif');
+cameraman_noisy = imnoise(cameraman, 'gaussian');
+lena = imread('../lena.tiff');
+lena_noisy = imnoise(lena, 'gaussian');
 
+PSNR(tire, tire_noisy)
+PSNR(cameraman, cameraman_noisy)
+PSNR(lena, lena_noisy)
