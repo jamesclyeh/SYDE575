eaf0caa187247748fc87b9ec4043f217eb57d890
jamesclyeh <jamesclyeh@gmail.com>
2014-10-22 15:04:25 -0400

    Finish part 2 and add frequnoisy.tff.

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -55,13 +55,49 @@ imshow(ifft2(exp(1*j*phase)), []);
 %% Load
 lena = im2double(rgb2gray(imread('../lena.tiff')));
 lena_gauss = imnoise(lena, 'gaussian', 0, 0.005);
-psnr(lena,lena_gauss);
+disp(strcat('PSNR oringinal vs gaussian noise: ', num2str(psnr(lena, lena_gauss))));
 
 %% Log fourier spectra
 fft_lena = fftshift(fft2(lena));
-log_fft_lena=log(abs(fft_lena));
+log_fft_lena = log(abs(fft_lena));
 fft_lena_gauss = fftshift(fft2(lena_gauss));
-log_fft_lena=log(abs(fft_lena_gauss));
+log_fft_lena_gauss = log(abs(fft_lena_gauss));
 
+figure(31);
 imshow(log_fft_lena, []);
-imshow(log_fft_lena, []);
+figure(32);
+imshow(log_fft_lena_gauss, []);
+
+%% Ideal low pass filter r = 60
+[height, width] = size(lena);
+r = 60;
+h = fspecial('disk', r); 
+h(h > 0) = 1;
+h_freq = zeros(height, width);
+h_freq([height / 2 - r: width / 2 + r], [width / 2 - r: width / 2 + r]) = h;
+
+low_pass_fft_lena_gauss_60 = real(ifft2(ifftshift(fft_lena_gauss .* h_freq)));
+figure(33);
+imshow(low_pass_fft_lena_gauss_60, []);
+disp(strcat('PSNR oringinal vs LPF denoised radius 60: ', num2str(psnr(lena, low_pass_fft_lena_gauss_60))));
+
+%% Ideal low pass filter r = 20
+r = 20;
+h = fspecial('disk', r); 
+h(h > 0) = 1;
+h_freq = zeros(height, width);
+h_freq([height / 2 - r: width / 2 + r], [width / 2 - r: width / 2 + r]) = h;
+
+low_pass_fft_lena_gauss_20 = real(ifft2(ifftshift(fft_lena_gauss .* h_freq)));
+figure(33);
+imshow(low_pass_fft_lena_gauss_20, []);
+disp(strcat('PSNR oringinal vs LPF denoised radius 20: ', num2str(psnr(lena, low_pass_fft_lena_gauss_20))));
+
+%% Gaussian filter std dev = 60
+h = fspecial('gaussian', [height width], 60);
+h_norm = (h - min(min(h))) ./ (max(max(h)) - min(min(h)));
+gauss_fft_lena_gauss = real(ifft2(ifftshift(fft_lena_gauss .* h_norm)));
+
+figure(34);
+imshow(gauss_fft_lena_gauss, []);
+disp(strcat('PSNR oringinal vs gaussian denoised: ', num2str(psnr(lena, gauss_fft_lena_gauss))));
