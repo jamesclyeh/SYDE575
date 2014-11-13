d6f0a01eb35e5b8f00577383cab243015b2f9da3
abdul-qureshi <abdulhqureshi@gmail.com>
2014-11-13 00:28:34 -0500

    Added Adaptive Filter part of lab

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -37,3 +37,49 @@ f_blur_noise_restored_weiner = deconvwnr(f_blur_noise, ifftshift(ifft2(h_freq)),
 figure(6);
 imshow(f_blur_noise_restored_weiner);
 disp(strcat('PSNR oringinal vs blurred with noise restored with weiner: ', num2str(psnr(f, f_blur_noise_restored_weiner))));
+
+%========================================
+% Adaptive Filtering
+%========================================
+
+%% Lee Filter
+degraded = im2double(imread('degraded.tif'));
+original = im2double(imread('cameraman.tif'));
+
+imshow(degraded);
+rect = getrect();
+
+selected = degraded(rect(1):rect(1)+rect(3), rect(2):rect(2)+rect(4));
+% selected(:,1:rect(1)) = 0;
+% selected(1:rect(2),:) = 0;
+% selected(:,rect(1)+rect(3):end) = 0;
+% selected(rect(2)+rect(4):end,:) = 0;
+noise_var = var(selected(:));
+
+local_mean = colfilt(degraded, [10,10], 'sliding', @mean);
+local_var = colfilt(degraded, [10,10], 'sliding', @var);
+
+K = zeros(256, 256);
+
+for i = 1:256
+    for j = 1:256
+        if noise_var > local_var(i,j)
+            K(i,j) = 0;
+        else
+            K(i,j) = 1 - (noise_var/local_var(i,j));
+        end
+    end
+end
+
+denoise = K.*degraded + (1 - K).*local_mean;
+figure;
+imshow(denoise);
+figure;
+imshow(original);
+
+psnr = psnr(original, denoise);
+%% Gaussian Low Pass Filter
+h_g = fspecial('gaussian', [3 3],30);
+gaussian = imfilter(degraded, h_g, 'replicate');
+figure;
+imshow(gaussian);
